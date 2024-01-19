2e7fd7b9579c7a5552d81091dc35c338e663156b
Blaj Madalin <madalinblaj89@gmail.com>
2024-01-19 12:26:07 +0200

    last edit

--- Set1FP16/Program.cs
+++ Set1FP16/Program.cs
@@ -1,11 +1,50 @@
-﻿using System;
-
-namespace Program;
-
-class Solution
+﻿class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-       //solve LWO
+        int a, b, c, d, e;
+        Console.Write("a = ");
+        a = int.Parse(Console.ReadLine());
+        Console.Write("b = ");
+        b = int.Parse(Console.ReadLine());
+        Console.Write("c = ");
+        c = int.Parse(Console.ReadLine());
+        Console.Write("d = ");
+        d = int.Parse(Console.ReadLine());
+        Console.Write("e = ");
+        e = int.Parse(Console.ReadLine());
+
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (a > b)
+            {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+            if (b > c)
+            {
+                int temp = b;
+                b = c;
+                c = temp;
+            }
+            if (c > d)
+            {
+                int temp = c;
+                c = d;
+                d = temp;
+            }
+            if (d > e)
+            {
+                int temp = d;
+                d = e;
+                e = temp;
+            }
+        }
+        Console.WriteLine("{0} {1} {2} {3} {4}", a, b, c, d, e);
+
     }
+
+
 }
--- Set1FP20/Program.cs
+++ Set1FP20/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program 
+{
+    static void Main()
+    {
+        Console.Write("Introduceti numaratorul (m): ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Introduceti numitorul (n): ");
+        int n = int.Parse(Console.ReadLine());
+
+        string rezultat = ConversieDecimala(m, n);
+        Console.WriteLine($"Rezultat: {rezultat}");
+    }
+
+    static string ConversieDecimala(int numarator, int numitor)
+    {
+        int catul = numarator / numitor;
+        int restul = numarator % numitor;
+
+        if (restul == 0)
+        {
+            return catul.ToString();
+        }
+
+        string rezultat = catul.ToString() + ".";
+        string ParteIntreaga = rezultat;
+
+        Dictionary<int, int> pozitiiRest = new Dictionary<int, int>();
+
+        restul *= 10;
+        while (restul != 0)
+        {
+            if (pozitiiRest.ContainsKey(restul))
+            {
+                int pozitiePerioada = pozitiiRest[restul];
+                string parteNonPerioada = rezultat.Substring(2, pozitiePerioada - 2);
+                string partePerioada = rezultat.Substring(pozitiePerioada, rezultat.Length - pozitiePerioada);
+                return $"{ParteIntreaga}{parteNonPerioada}({partePerioada})";
+            }
+
+            pozitiiRest.Add(restul, rezultat.Length);
+
+            int catulNou = restul / numitor;
+            rezultat += catulNou.ToString();
+            restul = (restul % numitor) * 10;
+        }
+
+        return rezultat;
+    }
+}
